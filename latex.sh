8aed41da6681d33a4154f74fbf8d2e97d222bc29
Arjun Guha <a.guha@northeastern.edu>
2021-03-26 08:59:28 -0400

    another example

--- latex.sh
+++ latex.sh
@@ -1,14 +1,14 @@
 #!/bin/sed -f
 s/fun/λ/g
-s/any/\*/g
+s/any/\\dyn/g
 s/\.gtlc//g
 s/_/ /g
 s/ : /:/g
 s/λ /λ/g
-s/λ/\\lambda /g
+s/λ/\\kw{fun} /g
 s/->/\\rightarrow /g
 s/BEGIN/\\begin{array}{l|l}/g
 s/migeed/MaxMigrate/g
 s/ins-and-outs/InsAndOuts/g
-s/smt/Our tool/g
+s/smt/\\ourtool/g
 s/no-context/PreciseMigrate/g
